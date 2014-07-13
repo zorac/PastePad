09f3ecc5a779b2b59a40a3abaa7b4802829d38ea
Mark Rigby-Jones <mark@rigby-jones.net>
2014-07-13 08:52:23 +0100

    Initial Commit

---
+++ PastePad/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PastePad
+//
+//  Created by Mark Rigby-Jones on 13/07/2014.
+//  Copyright (c) 2014 Mark Rigby-Jones. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PastePad/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PastePad
+//
+//  Created by Mark Rigby-Jones on 13/07/2014.
+//  Copyright (c) 2014 Mark Rigby-Jones. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
